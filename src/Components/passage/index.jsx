e1e79442d8e540bfe18e62cfe2c028f25cdd8ba7
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2022-02-20 19:09:13 +0530

    feat: working para-questions
    Signed-off-by: Mukesh Gurpude <mukeshgurpude02@gmail.com>

--- src/Components/passage/index.jsx
+++ src/Components/passage/index.jsx
@@ -1,5 +1,6 @@
 import { useReducer } from 'react'
 import TextInput from '../textarea'
+import { get_answer } from '../../api/passage'
 import './style.scss'
 
 function reducer(state, action) {
@@ -10,13 +11,26 @@ function reducer(state, action) {
       return {...state, question: action.value}
     case 'set_answer':
       return {...state, answer: action.value}
+    case 'loading':
+      return {...state, loading: action.value}
     default:
       return state
   }
 }
 
 export default function PassageAnswers() {
-  const [state, dispatch] = useReducer(reducer, { context: '', question: '', loading: '', answer: '' })
+  const [state, dispatch] = useReducer(reducer, { context: '', question: '', loading: false, answer: '' })
+
+  function fetch_answer() {
+    if(!state.question || !state.context) return
+    dispatch({type: 'loading', value: true})
+    get_answer(state.context, state.question)
+      .then((res) => {
+        dispatch({type: 'set_answer', value: res.body.output})
+      })
+      .catch(err => dispatch({type: 'set_answer', value: err.message}))
+      .finally(() => dispatch({type: 'loading', value: false}))
+  }
 
   return <>
     <section>
@@ -29,7 +43,10 @@ export default function PassageAnswers() {
           value={state.context}
           max_length={1024}
           fullWidth
-          onchange={(value) => dispatch({type: 'context_change', value})}
+          onchange={(value) => {
+            dispatch({type: 'set_answer', value: ''})
+            dispatch({type: 'context_change', value})
+          }}
         />
       </div>
       <div>
@@ -42,9 +59,18 @@ export default function PassageAnswers() {
           value={state.question}
           onchange={(value) => dispatch({type: 'question_change', value})}
         />
-        <button>Get Answer</button>
+        <button onClick={fetch_answer} >Get Answer</button>
       </div>
     </section>
-    <p>{state.answer}</p>
+    {
+      state.loading ? <div className='output'><p>Loading...</p></div>
+      : (
+      (state.answer && state.question) ?
+      <div className='output' data-empty={(state.answer === '')}>
+        <p>{state.question}</p>
+        <p>{state.answer}</p>
+      </div>
+      : null
+    )}
   </>
 }
