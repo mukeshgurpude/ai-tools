a271b3622397723f932a44e241e874d3f4817810
Sarang Wadode <sarangwadode@gmail.com>
2022-02-20 14:14:00 +0530

    Sidebar component created

---
+++ src/Components/Sidebar/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './style.css'
+
+
+export default function Sidebar() {
+    return (
+        <div className="pages">
+            <ul>
+                <li className="page">Extractive Q/A</li>
+                <li className="page">Summarizer</li>
+                <li className="page">Coming Soon</li>
+                <li className="page">Coming Soon</li>
+                <li className="page">Coming Soon</li>
+            </ul>
+        </div>
+    )
+}
